020581ab55047e19008ba6a652695f8521bdf6c4
Ekaterina Folkina <folkina7@gmail.com>
2021-10-21 17:28:10 +0300

    add new files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import './sass/main.scss';
+import menuTemplate from './templates/menu-template.hbs';
+import menu from './js/menu.json';
+
+const menuContainer = document.querySelector('js-menu');
+const cardsMarkup = createMenuCardsMarkup(menu);
+
+menuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// menuContainer.addEventListener('click', onMenuContainerClick);
+
+function createMenuCardsMarkup(menu) {
+  return menuTemplate(menu);
+}
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+// const refs = {
+//   body: document.querySelector('body'),
+//   themeSwitch: document.querySelector('#theme-switch-toggle'),
+// };
+
+// themeDefaultSetting();
+
+// refs.themeSwitch.addEventListener('change', onThemeSwitchChange);
+
+// function onThemeSwitchChange() {
+//   refs.body.classList.toggle(Theme.DARK);
+
+//   if (refs.body.classList.contains(Theme.DARK)) {
+//     localStorage.setItem('theme', Theme.DARK);
+//   } else {
+//     localStorage.setItem('theme', Theme.LIGHT);
+//   }
+// }
+
+// function themeDefaultSetting() {
+//   if (!localStorage.length) {
+//     localStorage.setItem('theme', Theme.LIGHT);
+//   }
+//   refs.body.classList.add(localStorage.getItem('theme'));
+// }
