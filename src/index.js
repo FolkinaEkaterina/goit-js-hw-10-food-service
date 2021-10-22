d4625ce2cf838ab1ef0efa871fffb1b03d52a754
Ekaterina Folkina <folkina7@gmail.com>
2021-10-22 15:18:40 +0300

    remove theme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
+import './js/theme.js';
 import menuTemplate from './templates/menu-template.hbs';
 import menu from './js/menu.json';
 
@@ -10,34 +11,3 @@ menuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 function createMenuCardsMarkup(menu) {
   return menuTemplate(menu);
 }
-
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
-// const refs = {
-//   body: document.querySelector('body'),
-//   themeSwitch: document.querySelector('#theme-switch-toggle'),
-// };
-
-// themeDefaultSetting();
-
-// refs.themeSwitch.addEventListener('change', onThemeSwitchChange);
-
-// function onThemeSwitchChange() {
-//   refs.body.classList.toggle(Theme.DARK);
-
-//   if (refs.body.classList.contains(Theme.DARK)) {
-//     localStorage.setItem('theme', Theme.DARK);
-//   } else {
-//     localStorage.setItem('theme', Theme.LIGHT);
-//   }
-// }
-
-// function themeDefaultSetting() {
-//   if (!localStorage.length) {
-//     localStorage.setItem('theme', Theme.LIGHT);
-//   }
-//   refs.body.classList.add(localStorage.getItem('theme'));
-// }
